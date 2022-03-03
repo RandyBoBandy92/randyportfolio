722af064ab95041f7ac4bf61c0195bc9f3386392
Ryan Gulak <ryan.gulak@gmail.com>
2022-03-02 16:07:43 -0800

    set up router to point to Home component

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/home/Home";
 import WorkShop from "../pages/workshop/Workshop";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<WorkShop />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
