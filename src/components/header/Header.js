4f0076c52edf8b7cc55797935d352465782e6b7c
Ryan Gulak <ryan.gulak@gmail.com>
2022-03-04 08:04:54 -0800

    imported images, refactoring components into folder structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import Logo from "../../assets/images/logo/white/favicon-32x32.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,34 +1,15 @@
-import { Link } from "react-router-dom";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import "./layout.scss";
 
 const Layout = () => {
   return (
     <>
-      <header>
-        <img src="" alt="" />
-      </header>
+      <Header />
       <main>
-        {/* want to embed https://github.dev/RandyBoBandy92/moviedatabase */}
-        <h1>Movie Database</h1>
-        {/* how do i embed that website within this page? */}
-        <iframe
-          style={{ height: "500px", width: "100%" }}
-          title="Movie Database"
-          src="https://github1s.com/RandyBoBandy92/moviedatabase"
-          frameborder="0"
-        />
+        <h1>I am content</h1>
       </main>
-      <footer>
-        <nav>
-          <ul>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Projects</li>
-            <li>
-              <a href="https://blog.randygulak.com">Blog</a>
-            </li>
-          </ul>
-        </nav>
-      </footer>
+      <Footer />
     </>
   );
 };
