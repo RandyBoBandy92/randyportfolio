b9954503060b89e24ea939ec1c45f04918751a76
Ryan Gulak <ryan.gulak@gmail.com>
2022-03-08 10:09:32 -0800

    icons added, experimenting with tech stack component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -19,9 +19,18 @@ const Post = ({ appId, postName }) => {
 
   return (
     <div className="post">
-      <div className="app-info">
+      <div className="app-info-header">
         <h2>App Info</h2>
         <AppIcon appData={appData} />
+      </div>
+      <div className="app-info tech">
+        <h3>Tech Stack</h3>
+        <ul>
+          <li>HTML 5</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
       </div>
       <Markdown
         options={{
