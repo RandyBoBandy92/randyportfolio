e828b2b79a12f60ff5473ae35ac14d86d14bc522
Ryan Gulak <ryan.gulak@gmail.com>
2022-03-07 16:46:43 -0800

    added copy popup

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,17 +1,26 @@
 import "./_contact.scss";
 import logoWhite from "../../assets/images/logo/white/logoFullResWhite.png";
 import copyIcon from "../../assets/images/icons/copy.svg";
+import { useState } from "react";
 
 const Contact = () => {
+  const [copyClick, setCopyClick] = useState(false);
   const email = "randy@randygulak.com";
   const copyEmail = () => {
     navigator.clipboard.writeText(email);
+    setCopyClick(true);
+    setTimeout(() => {
+      setCopyClick(false);
+    }, 1500);
   };
   return (
     <div className="contact">
       <span className="email-link">
         <a href={`mailto:${email}`}>{email}</a>
-        <button onClick={copyEmail} className="copy-email">
+        <button
+          onClick={copyEmail}
+          className={`copy-email${copyClick ? " active" : ""}`}
+        >
           <img src={copyIcon} alt="" />
         </button>
       </span>
