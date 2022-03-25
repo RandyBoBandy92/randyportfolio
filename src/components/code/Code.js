d7026d60bc5dc8a86d52c5e6a81be3afedb091e2
Ryan Gulak <ryan.gulak@gmail.com>
2022-03-25 09:40:06 -0700

    fixed pre tag error

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -4,6 +4,11 @@ import json from "react-syntax-highlighter/dist/esm/languages/prism/json";
 import vscDarkPlus from "./vsc-dark-plus";
 
 const Code = ({ children, className }) => {
+  // Because you can't have <pre> tags in a <p> tag, when I use
+  // syntax to refer to functions like this: `checkBounds()`
+  // I saw that this returns an empty className
+  // so I'm tapping into that to control whether or not to render <pre> tags.
+
   SyntaxHighlighter.registerLanguage("jsx", jsx);
   SyntaxHighlighter.registerLanguage("json", json);
   // i need to run a split on the className prop to get the language
@@ -14,6 +19,7 @@ const Code = ({ children, className }) => {
       className={className ? className : "inline-code"}
       language={language}
       style={vscDarkPlus}
+      PreTag={className ? "pre" : "span"}
     >
       {children}
     </SyntaxHighlighter>
